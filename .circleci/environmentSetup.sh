6f7acc38208f7e71d5d31cbc138b6da59170fa41
AILLERON\dojr <dojr@ailleron.com>
2018-07-30 16:38:19 +0200

    new Ci script

---
+++ .circleci/environmentSetup.sh
@@ -0,0 +1,15 @@
+function copyEnvVarsToGradleProperties {
+    GRADLE_PROPERTIES=$HOME"/.gradle/gradle.properties"
+    export GRADLE_PROPERTIES
+    echo "Gradle Properties should exist at $GRADLE_PROPERTIES"
+
+    if [ ! -f "$GRADLE_PROPERTIES" ]; then
+        echo "Gradle Properties does not exist"
+
+        echo "Creating Gradle Properties file..."
+        touch $GRADLE_PROPERTIES
+
+        echo "Writing TEST_API_KEY to gradle.properties..."
+        echo "TEST_API_KEY=$TEST_API_KEY_ENV_VAR" >> $GRADLE_PROPERTIES
+    fi
+}
